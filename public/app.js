aba6e4e4cf716d01ce4a2a82f17541d3f60147dc
mwritter <mritter2283@gmail.com>
2020-05-20 17:55:22 -0400

    mm dont think I got the dynamic import thing working yet

--- public/app.js
+++ public/app.js
@@ -1,19 +1,15 @@
 // look into using router
 // https://www.freecodecamp.org/news/making-vanilla-js-router-in-javascript/
-
+import Battleship from './games/Battleship/Battleship.js'
 let games = document.getElementsByClassName('game-link')
-let self = this;
 for (let game of games) {
 	game.addEventListener('click', () => {
-		import(`./games/${game.dataset.value}/${game.dataset.value}.js`)
-			.then((mod) => {
-				document.getElementById('game-select').remove();
-				const names = {
-					playerOne: "Matthew",
-					playerTwo: "Player 2"
-				}
-				const game = new mod.default(names);
-				game.go();
-			})
+		document.getElementById('game-select').remove();
+		const names = {
+			playerOne: "Matthew",
+			playerTwo: "Player 2"
+		}
+		const newGame = new Battleship(names);
+		newGame.go();
 	})
 }
